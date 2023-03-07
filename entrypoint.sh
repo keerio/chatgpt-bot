9bd2acf54d6217861531dea6938b8ee5b3b9be32
keerio <keerio@gmail.com>
2023-03-07 12:06:14 +0300

    \\

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,20 +3,27 @@
 #set -e
 
 # Run joplin config -v
-joplin config -v &
+#joplin config -v &
+
+export JO_API_TOKEN=$(joplin config api.token)
+
+
+
+
+
+socat -d -d tcp-listen:41900,reuseaddr,fork tcp:localhost:41184 &
+
 ## Install variables 
 
-joplin config sync.target 9 \
-joplin config clipperServer.autoStart = true \
-joplin config sync.9.path $SYNCPATH \
-joplin config sync.9.username $SYNCNAME \
-joplin config sync.9.password $SYNCPASSWORD
 
-# Export token
-export JO_API_TOKEN=$(joplin config api.token)
+#joplin config clipperServer.autoStart = true \
+joplin config sync.9.path $SYNCPATH
+joplin config sync.9.username $SYNCNAME
+joplin config sync.9.password $SYNCPASSWORD
+joplin config sync.target 9 \
 
 joplin sync 9
 
 joplin server start &
 
-socat -d -d tcp-listen:41900,reuseaddr,fork tcp:localhost:41184 &
+# Export token
